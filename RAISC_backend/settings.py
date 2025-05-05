6120a95087f603ca8784aba4213acdfaede90fa4
Hammad Ahmed <bosshammad0@gmail.com>
2025-05-06 03:14:04 +0500

    railway in allowed host

--- RAISC_backend/settings.py
+++ RAISC_backend/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = config('SECRET_KEY', default='your-secret-key')
 DEBUG = True  # Set to False in production
 APPEND_SLASH = False
 
-ALLOWED_HOSTS = ['your-railway-app.up.railway.app', 'localhost']
+ALLOWED_HOSTS = ['web-production-deb22.up.railway.app', 'localhost']
 
 # Application definition
 INSTALLED_APPS = [
