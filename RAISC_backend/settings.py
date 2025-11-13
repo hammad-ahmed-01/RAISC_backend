e6016488e01c084b397c0718c6ba3a8f52668a37
hammad-datapulse <hahmed@datapulsetechnologies.org>
2025-11-13 22:50:51 +0500

    deploying final

--- RAISC_backend/settings.py
+++ RAISC_backend/settings.py
@@ -24,13 +24,7 @@ DEBUG = True  # Set to False in production
 APPEND_SLASH = False
 
 # ALLOWED_HOSTS = ['web-production-deb22.up.railway.app', 'localhost']
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    '0.0.0.0',
-    'localhost:3000',
-    '127.0.0.1:3000'
-] 
+ALLOWED_HOSTS = ['*']  
 
 # Application definition
 INSTALLED_APPS = [
@@ -67,6 +61,8 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "https://stage.raisc.org/",
+    "https://raisc.org/",
 ]
 
 ROOT_URLCONF = 'RAISC_backend.urls'
