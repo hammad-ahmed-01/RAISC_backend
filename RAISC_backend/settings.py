680cb088117da2445b8d362aa2059f354909f819
yusra2526 <yusrazainab.xb.senior@gmail.com>
2026-02-10 20:46:52 +0500

    Organization updated

--- RAISC_backend/settings.py
+++ RAISC_backend/settings.py
@@ -6,13 +6,13 @@ import dj_database_url
 
 load_dotenv()
 
-# DATABASES = {
-#     'default': dj_database_url.parse(
-#         os.getenv("DATABASE_URL"),
-#         conn_max_age=0,                     # important with transaction pooling
-#         ssl_require= False
-#     )
-# }
+DATABASES = {
+    'default': dj_database_url.parse(
+        os.getenv("DATABASE_URL"),
+        conn_max_age=0,                     # important with transaction pooling
+        ssl_require= False
+    )
+}
 
 # Build paths inside the project
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,20 +83,20 @@ TEMPLATES = [
     },
 ]
 
-# WSGI_APPLICATION = 'RAISC_backend.wsgi.application'
-# ASGI_APPLICATION = 'RAISC_backend.asgi.application'
+WSGI_APPLICATION = 'RAISC_backend.wsgi.application'
+ASGI_APPLICATION = 'RAISC_backend.asgi.application'
 
 # Database Configurations
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('POSTGRES_DB', default='RAISC_DB'),
-        'USER': config('POSTGRES_USER', default='postgres'),
-        'PASSWORD': config('POSTGRES_PASSWORD', default='123'),
-        'HOST': config('POSTGRES_HOST', default='localhost'),
-        'PORT': config('POSTGRES_PORT', default='5432'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('POSTGRES_DB', default='RAISC_DB'),
+#         'USER': config('POSTGRES_USER', default='postgres'),
+#         'PASSWORD': config('POSTGRES_PASSWORD', default='local1234'),
+#         'HOST': config('POSTGRES_HOST', default='localhost'),
+#         'PORT': config('POSTGRES_PORT', default='5432'),
+#     }
+# }
 
 REDIS_HOST = config('REDIS_HOST', default='localhost')
 REDIS_PORT = config('REDIS_PORT', default='6379')
